e078d084cdeef8e2a24aa7440b6ca8dc02228edd
Muhammad Yahya <zulfikeryahya@gmail.com>
2023-09-24 00:46:27 +0600

    succesfully sonnected to mongodb

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const cors = require('cors');
+const port = process.env.PORT || 5000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+   res.send("ok everythisn")
+})
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.p43vn94.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   //  await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.listen(port, (req, res)=>{
+   console.log(`The server is running on the port of ${port}`)
+})
